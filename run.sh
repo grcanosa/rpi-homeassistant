fa7aaed4471fd092eb603ca346aafa7e606adeac
grcanosa <grcanosa@gmail.com>
2019-07-28 15:40:51 +0000

    Added run script

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+TTY=$(ls /dev/ttyACM*)
+echo $TTY
+sed s#TTYDEVICE#$TTY#g template-docker-compose.yml > no-commit-docker-compose.yaml
+docker-compose -f no-commit-docker-compose.yaml down
+docker container prune
+docker-compose -f no-commit-docker-compose.yaml up -d
