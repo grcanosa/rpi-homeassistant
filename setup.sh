65224b7ef051611295a2f6ab99085999f4ac658a
grcanosa <grcanosa@gmail.com>
2019-09-30 19:09:25 +0200

    Added calibre-web

--- setup.sh
+++ setup.sh
@@ -4,4 +4,6 @@ git clone https://github.com/Koenkk/zigbee-shepherd-converters.git ./zigbee2mqtt
 mkdir -p zigbee2mqtt/lib/extension/
 wget https://raw.githubusercontent.com/Koenkk/zigbee2mqtt/master/lib/extension/homeassistant.js -O ./zigbee2mqtt/lib/extension/homeassistant.js
 
+docker network create --driver=bridge --subnet=172.100.50.0/24 --ip-range=172.100.50.0/24 --gateway=172.100.50.1 nginx_network
+
 mkdir -p mosquitto/data
